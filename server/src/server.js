646d16116dedeecb09e9ba6eee7bbff0bb2260ca
Heron Yang <heron.yang.tw@gmail.com>
2015-03-29 01:27:50 -0400

    fix cross-site error while testing client side

--- server/src/server.js
+++ server/src/server.js
@@ -11,6 +11,16 @@ var auth = require('./auth')(config.appID, config.appSecret, config.dbConnect);
 
 var app = express();
 app.use (bodyParser());
+
+// FIXME: this is for allowing our testing client (localhost:8001) to access server
+// with out having cross-site error. Make sure it configs correctly when deployed.
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  next();
+});
+
 app.post('/api/login', auth.login.bind(auth));
 
 var httpServer = http.createServer(app);
