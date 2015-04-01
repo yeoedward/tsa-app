c78ca4bbb1fcc15b4fffd5546a786ef70461a3c9
Heron Yang <heron.yang.tw@gmail.com>
2015-03-31 22:32:32 -0400

    add api prototypes

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,11 @@
 /* FIXME: make sure this matches to the correct server location */
 var API_URL = "http://localhost:8001/api";
 
+/*
+ * ==============================================
+ * Facebok API
+ * ==============================================
+ */
 window.fbAsyncInit = function() {
   FB.init({
       appId      : '841727872531922',
@@ -82,7 +87,12 @@ function connectedCallback() {
   });
 }
 
-// tsa-api api login
+/*
+ * ==============================================
+ * TSA App API
+ * ==============================================
+ */
+// POST /api/login
 function apiLogin(accessToken, userID) {
   $.ajax({
       method: "POST",
@@ -99,13 +109,161 @@ function apiLogin(accessToken, userID) {
   });
 }
 
-// Update UI after successful login
+// POST /api/logout
+function apiLogin() {
+  $.ajax({
+      method: "POST",
+      url: API_URL + "/logout",
+      data: { },
+      success: function (result) {
+        if (result.success) {
+          console.log(result);
+          loggedOutSucceed();
+        } else {
+          console.log(result.message);
+        }
+      }
+  });
+}
+
+
+// GET /api/my_buddy
+function apiMyBuddy() {
+  $.ajax({
+      method: "GET",
+      url: API_URL + "/my_buddy",
+      data: { },
+      success: function (result) {
+        if (result.success) {
+          console.log(result);
+          updateMyBuddy(result['data']);
+        } else {
+          console.log(result.message);
+        }
+      }
+  });
+}
+
+// GET /api/buddies
+function apiBuddies() {
+  $.ajax({
+      method: "GET",
+      url: API_URL + "/buddies",
+      data: { },
+      success: function (result) {
+        if (result.success) {
+          console.log(result);
+          updateBuddies(result['data']);
+        } else {
+          console.log(result.message);
+        }
+      }
+  });
+}
+
+// GET /api/completed
+function apiCompleted(){
+  $.ajax({
+      method: "GET",
+      url: API_URL + "/completed",
+      data: { },
+      success: function (result) {
+        if (result.success) {
+          console.log(result);
+          updateCompleted(result['data']);
+        } else {
+          console.log(result.message);
+        }
+      }
+  });
+}
+
+// GET /api/tasks
+function apiTasks(){
+  $.ajax({
+      method: "GET",
+      url: API_URL + "/tasks",
+      data: { },
+      success: function (result) {
+        if (result.success) {
+          console.log(result);
+          updateTasks(result['data']);
+        } else {
+          console.log(result.message);
+        }
+      }
+  });
+}
+
+// POST /api/checkin
+function apiCheckin(taskNum){
+  $.ajax({
+      method: "POST",
+      url: API_URL + "/checkin",
+      data: { },
+      success: function (result) {
+        if (result.success) {
+          console.log(result);
+          // TODO: redirect to history tab
+          window.location.href = "/#history";
+        } else {
+          console.log(result.message);
+        }
+      }
+  });
+}
+
+
+/*
+ * ==============================================
+ * API Event Callbacks
+ * ==============================================
+ */
 function loggedInSucceed() {
   var time_interval = 500;
   $('#tsa-welcome').fadeOut(time_interval);
   $('#tsa-content').delay(time_interval).fadeIn();
 }
 
+function loggedOutSucceed() {
+  // refresh current page (will direct to login page)
+  location.reload();
+}
+
+function updateMyBuddy(data) {
+  // TODO: update my buddy, should have:
+  // - name
+  // - FB url
+  // - profile image
+}
+
+function updateBuddies(data) {
+  // TODO: put the recieved data into the table, should have following in each row:
+  // - # rank
+  // - buddy 1 (name, facebook url, profile image url)
+  // - buddy 2 (name, facebook url, profile image url)
+  // - # tasks
+}
+
+function updateCompleted(data) {
+  // 2 tables will be updated using input data
+  // TODO: put data into 2 tables, shoud have following in each completed task:
+  // - task name
+  // - time
+  // - status (string or defined enum number)
+}
+
+function updateTasks(data) {
+  // TODO: get a list of tasks and update it into the download option list, for each task:
+  // - the name of the task
+  // - index of the task
+}
+
+/*
+ * ==============================================
+ * GUI Event Callbacks
+ * ==============================================
+ */
 // Event handler for login button
 function fbLogin() {
 
@@ -116,4 +274,21 @@ function fbLogin() {
       alarm("WHY? Just Login!");
     }
   });
+
 }
+
+/*
+ * ==============================================
+ * Main Function
+ * ==============================================
+ */
+$( document ).ready(function() {
+  /*
+  apiMyBuddy();
+  apiBuddies()
+  apiCompleted();
+  apiTasks()
+  */
+
+  // add apiCheckin to button listener
+});
